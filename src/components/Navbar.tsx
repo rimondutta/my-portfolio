adf79a915ecf5acd7b0e7d324c3d77bd06c4fb1b
Rimon Dutta <darklegend12359@gmail.com>
2026-03-10 19:25:02 +0600

    Increase_scroll_smoothness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,8 @@ const Navbar = () => {
     smoother = ScrollSmoother.create({
       wrapper: "#smooth-wrapper",
       content: "#smooth-content",
-      smooth: 1.7,
-      speed: 1.7,
+      smooth: 2.8,
+      speed: 1.1,
       effects: true,
       autoResize: true,
       ignoreMobileResize: true,
