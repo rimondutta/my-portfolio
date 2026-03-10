d5086515f6036653577aec171b0879692323b1f8
Rimon Dutta <darklegend12359@gmail.com>
2026-03-10 19:37:57 +0600

    Final_TechStack_fix_Corrected_asset_paths

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -199,7 +199,7 @@ const TechStack = () => {
           ))}
         </Physics>
         <Environment
-          files="/models/char_enviorment.hdr"
+          files="/models/env_map.hdr?v=1"
           environmentIntensity={0.5}
           environmentRotation={[0, 4, 2]}
         />
