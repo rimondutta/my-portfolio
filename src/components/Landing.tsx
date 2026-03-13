142a47ff66ce2a27b1882ab96a5be45f0f1f5d4a
Rimon Dutta <darklegend12359@gmail.com>
2026-03-13 20:37:08 +0600

    revert: landing text to old state

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -15,14 +15,15 @@ const Landing = ({ children }: PropsWithChildren) => {
             </h1>
           </div>
           <div className="landing-info">
+            <h3>A Full Stack</h3>
             <h2 className="landing-info-h2">
               <div className="landing-h2-1">Developer</div>
               <div className="landing-h2-2">Engineer</div>
             </h2>
-            <h3 className="landing-info-sub">
-              <div className="landing-sub-1">Engineer</div>
-              <div className="landing-sub-2">Developer</div>
-            </h3>
+            <h2>
+              <div className="landing-h2-info">Engineer</div>
+              <div className="landing-h2-info-1">Developer</div>
+            </h2>
           </div>
         </div>
         {children}
